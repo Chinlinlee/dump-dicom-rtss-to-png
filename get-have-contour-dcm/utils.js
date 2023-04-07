201fdf16442b6532701befeddd898c1bb318d44d
chin <a5566qq2581@gmail.com>
2023-04-08 01:45:48 +0800

    feat: make dcm2json async
    - When dcm2json not async may cause dicom utf8 convert not perform
    - Make it be asynchronous

--- get-have-contour-dcm/utils.js
+++ get-have-contour-dcm/utils.js
@@ -1,7 +1,7 @@
 const { dcm2json } = require("dicom-to-json");
 const { DicomUtf8Converter } = require("../dcm4che/DicomUtf8Converter");
 
-async function dcm2JsonNative(filename) {
+async function dcm2jsonAsync (filename) {
     return new Promise((resolve, reject) => {
         try {
             dcm2json(filename, function (dicomJsonStr) {
@@ -11,27 +11,24 @@ async function dcm2JsonNative(filename) {
                 return resolve(dicomJson);
             });
         } catch(e) {
-            if (e.message.includes("EXITCODE_CANNOT_CONVERT_TO_UNICODE")) {
-                console.log(`The file: ${filename} may missing/incorrect (0008,0005) charset, converter dicom to UTF8`);
-                try {
-                    let dicomUtf8Converter = new DicomUtf8Converter(filename);
-                    dicomUtf8Converter.convert().then(() => {
-                        dcm2json(filename, function (dicomJsonStr) {
-                            dicomJsonStr = dicomJsonStr.replace(/,\\u0000/g, '');
-                            dicomJsonStr = dicomJsonStr.replace(/\\u0000/g, '');
-                            let dicomJson = JSON.parse(dicomJsonStr);
-                            return resolve(dicomJson);
-                        });
-                    });
-                } catch(e) {
-                    throw e;
-                }
-            }
-            console.error(e);
             return reject(e);
         }
-        
     });
+}
+
+async function dcm2JsonNative(filename) {
+    try {
+        return await dcm2jsonAsync(filename);
+    } catch(e) {
+        if (e.message.includes("EXITCODE_CANNOT_CONVERT_TO_UNICODE")) {
+            console.log(`The file: ${filename} may missing/incorrect (0008,0005) charset, converter dicom to UTF8`);
+            let dicomUtf8Converter = new DicomUtf8Converter(filename);
+            await dicomUtf8Converter.convert();
+            return await dcm2jsonAsync(filename);
+        }
+        console.error(e);
+        throw e;
+    }
 }
 
 module.exports.dcm2JsonNative = dcm2JsonNative;
